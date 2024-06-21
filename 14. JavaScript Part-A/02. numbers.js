9a5dc2ad84d1583b33d3600634aabdda2495cfd9
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2024-06-21 08:28:04 +0530

    Completed JavaScript Part-C

--- 14. JavaScript Part-A/02. numbers.js
+++
@@ -1,6 +0,0 @@
-// In JS Number data type stores all positive, negative and floating points digits.
-// It Stores numbers with absolute value less that 2^53.
-
-a = 16;
-console.log(a);
-console.log(typeof a);
