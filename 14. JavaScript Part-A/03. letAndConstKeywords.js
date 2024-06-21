91e8433aa5e7803d688eb1c89f30e83b9acaebaf
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2024-06-21 08:33:58 +0530

    Completed JavaScript Part-A

---
+++ 14. JavaScript Part-A/03. letAndConstKeywords.js
@@ -0,0 +1,14 @@
+// The let Keyword Is Used To Simply Decare A Variable
+let age = 23;
+let names = 'Alan';
+console.log(typeof names);
+let c;
+console.log(c);        // Undefined
+console.log(typeof c); // Undefined
+console.log("\n");
+
+
+// Const Keyword : Used Declare And Assign Values To constsnts.
+const k = 100;
+console.log(typeof k)
+console.log("\n");
---
+++ 14. JavaScript Part-A/06. strings.js
@@ -0,0 +1,14 @@
+let fullName =  "Akshat Rauthan";
+console.log(fullName);
+
+let char = 'a';
+console.log(char);
+
+let empty = "";
+console.log(empty);
+
+// Strings Are 0 based Indexed In Javastring.
+
+console.log(fullName[10]);
+console.log(fullName.length);
+console.log(fullName[15]);
