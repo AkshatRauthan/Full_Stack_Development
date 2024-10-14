9368234ddc68bcfd92951fe39cc034c9a6523a27
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2024-10-14 17:01:11 +0530

    Forst COmmit

--- Major Project/init/data.js
+++ Major Project/init/data.js
@@ -2,7 +2,7 @@ module.exports.data = [
   {
     title: "Cozy Beachfront Cottage",
     description:
-      "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
+      "Esc  ape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
     image: {
       filename: "listingimage",
       url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
