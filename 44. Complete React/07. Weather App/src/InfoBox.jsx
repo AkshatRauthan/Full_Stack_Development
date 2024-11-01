ad7972200b2105372a5d06984d4ad058eb7fc8c6
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2024-11-01 23:31:46 +0530

    Completed React Weather App

--- 44. Complete React/07. Weather App/src/InfoBox.jsx
+++ 44. Complete React/07. Weather App/src/InfoBox.jsx
@@ -3,15 +3,18 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
-let imgUrl = {
-    winter : "https://images.unsplash.com/photo-1477601263568-180e2c6d046e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    haze : "https://images.unsplash.com/photo-1680352267694-a7fd4c33d4e1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    summer : "https://images.unsplash.com/uploads/14121010130570e22bcdf/e1730efe?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    rainy : "https://plus.unsplash.com/premium_photo-1676212747574-c401d596e853?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-}
+let imgName = 'nice-day';
+
 const capitalize = s => s && String(s[0]).toUpperCase() + String(s).slice(1).toLowerCase();
 
 export default function InfoBox({result}){
+    let arr = ['clear-day', 'clear-night', 'cloudy', 'fog', 'hail', 'rain', 'sleet', 'snow', 'thunder', 'wind'];
+    for (let str of arr){
+        if (str === result.icon || result.icon.includes(str)){
+            imgName = str;
+            break;
+        }
+    }
     return (
     <div className="InfoBox">
         <Typography variant="h5" component="div">
@@ -20,7 +23,7 @@ export default function InfoBox({result}){
         <Card sx={{ minWidth: 275}}>
             <CardMedia
                 sx={{ height: 120 }}
-                image={(result.temp<=15)?imgUrl.winter:(result.temp>=25)?imgUrl.summer:(result.humidity>=75)?imgUrl.rainy:imgUrl.haze}
+                image={`./src/assets/cardImages/${imgName}.jpg`}
             />
             <CardContent>            
                 <img src={`./src/assets/weatherIcons/${result.icon}.png`} alt={result.icon} />
