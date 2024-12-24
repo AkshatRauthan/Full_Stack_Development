604ad7ec3720bce95b03d3bfc9967f2747c1b416
Akshat_Rauthan <141433398+AkshatRauthan@users.noreply.github.com>
2024-12-24 11:38:43 +0530

    Updates

--- 45. React Projects/01. Tic Tac Toe/src/App.jsx
+++ 45. React Projects/01. Tic Tac Toe/src/App.jsx
@@ -1,7 +1,11 @@
 import "./styles/App.css";
 import TicTacToe from "./components/TicTacToe.jsx";
 function App() {
-    return <TicTacToe />;
+    return (
+        <div className="ticTacToe">
+            <TicTacToe />
+        </div>
+    )
 }
 
 export default App;
